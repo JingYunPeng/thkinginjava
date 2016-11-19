2b1a0ac9e0a5d673176697687d096da57e2b1318
Administrator <310491287@qq.com>
2016-11-20 00:02:46 +0800

    init

---
+++ src/main/java/prep/jing/study/thkinginjava/App.java
@@ -0,0 +1,13 @@
+package prep.jing.study.thkinginjava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
